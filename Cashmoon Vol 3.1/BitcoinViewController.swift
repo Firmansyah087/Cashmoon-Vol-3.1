9ec1694e4bb02f76af5e11d311d46279318ae336
MrBorkim <siwecki.maks@gmail.com>
2018-04-05 23:39:36 +0200

    CoinPot Faucet

---
+++ Cashmoon Vol 3.1/BitcoinViewController.swift
@@ -0,0 +1,38 @@
+//
+//  BitcoinViewController.swift
+//  Cashmoon Vol 3.1
+//
+//  Created by Maksymilian Siwecki on 05.04.2018.
+//  Copyright © 2018 Maksymilian Siwecki. All rights reserved.
+//
+
+import UIKit
+
+class BitcoinViewController: UIViewController {
+
+    @IBOutlet weak var Web: UIWebView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        let url = URL(string: "https://moonbit.co.in/?ref=4ffebd18902d")
+        Web.loadRequest(URLRequest(url: url!))
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Cashmoon Vol 3.1/DogeViewController.swift
@@ -0,0 +1,38 @@
+//
+//  DogeViewController.swift
+//  Cashmoon Vol 3.1
+//
+//  Created by Maksymilian Siwecki on 05.04.2018.
+//  Copyright © 2018 Maksymilian Siwecki. All rights reserved.
+//
+
+import UIKit
+
+class DogeViewController: UIViewController {
+
+    @IBOutlet weak var Web: UIWebView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        let url = URL(string: "https://moondoge.co.in/?ref=404c088bb6bd")
+        Web.loadRequest(URLRequest(url: url!))
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
